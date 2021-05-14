41be1d16cc5f5857dabfbddd1f0ca3fad801709f
Grasiela Gomes <produtoradama@gmail.com>
2021-05-14 16:30:47 -0300

    Delete category

--- backend/src/main/java/com/damaprodutora/DsCatalog/services/CategoryService.java
+++ backend/src/main/java/com/damaprodutora/DsCatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.damaprodutora.DsCatalog.dto.CategoryDTO;
 import com.damaprodutora.DsCatalog.entities.Category;
 import com.damaprodutora.DsCatalog.repositories.CategoryRepository;
+import com.damaprodutora.DsCatalog.services.exceptions.DatabaseException;
 import com.damaprodutora.DsCatalog.services.exceptions.ResourceNotFoundedException;
 
 @Service
@@ -51,8 +54,20 @@ public class CategoryService {
 			return new CategoryDTO(entity);
 		}
 		catch (EntityNotFoundException e) {
-			throw new ResourceNotFoundedException("Id not found" + id);
+			throw new ResourceNotFoundedException("Id not found " + id);
 		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundedException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}	
 	}
 
 }
