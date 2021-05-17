fa8c1cbe840d56f75a08f9e35986707d5abdf72e
Grasiela Gomes <produtoradama@gmail.com>
2021-05-17 15:10:56 -0300

    Product repository service resource get ok

---
+++ backend/src/main/java/com/damaprodutora/DsCatalog/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.damaprodutora.DsCatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.damaprodutora.DsCatalog.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
