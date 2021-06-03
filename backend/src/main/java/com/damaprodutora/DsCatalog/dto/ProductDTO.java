d238bc02aae6169504c0709fa0b56ea43d30d8be
Grasiela Gomes <produtoradama@gmail.com>
2021-06-03 15:35:05 -0300

    custom constraint validator

--- backend/src/main/java/com/damaprodutora/DsCatalog/dto/ProductDTO.java
+++ backend/src/main/java/com/damaprodutora/DsCatalog/dto/ProductDTO.java
@@ -20,7 +20,7 @@ public class ProductDTO implements Serializable {
 	
 	private Long id;
 	
-	@Size(min = 5, max = 60, message = "O nome deve ter no mínimo 5 e no máximo 60 letras")
+	@Size(min = 5, max = 60, message = "O nome deve ter no mínimo 5 e no máximo 60 caracteres")
 	@NotBlank(message = "Campo obrigatório")
 	private String name;
 	
