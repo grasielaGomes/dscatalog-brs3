c8c4233d661124f1853bcaf0c5f0691999264e22
Grasiela Gomes <produtoradama@gmail.com>
2021-08-27 11:37:02 -0300

    styling navbar mobile

--- frontweb/src/components/Navbar/navbar.tsx
+++ frontweb/src/components/Navbar/navbar.tsx
@@ -1,12 +1,25 @@
 import './navbar-styles.scss';
+import 'bootstrap/js/src/collapse.js';
 
 const Navbar = () => (
-  <nav className="bg-primary navbar navbar-expand-md">
+  <nav className="bg-primary navbar navbar-expand-md navbar-dark">
     <div className="container-fluid">
       <a href="link">
         <h4 className="nav-logo-text">DS Catalog</h4>
       </a>
-      <div className="collapse navbar-collapse">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#dscatalog-navbar"
+        aria-controls="dscatalog-navbar"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="dscatalog-navbar">
         <ul className="navbar-nav main-menu">
           <li>
             <a href="link">HOME</a>
