29ae60cc1fbecf5ccafa55d836d00fe70f586316
Grasiela Gomes <produtoradama@gmail.com>
2021-09-06 09:21:32 -0300

    Product detail route

--- frontweb/src/Routes.tsx
+++ frontweb/src/Routes.tsx
@@ -1,6 +1,7 @@
 import Navbar from 'components/Navbar/navbar';
 import Catalog from 'pages/Catalog/catalog';
 import Home from 'pages/Home/home';
+import ProductDetail from 'pages/ProductDetail/product-detail';
 import { Admin } from 'pages/Admin/admin';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
@@ -11,8 +12,11 @@ export const Routes = () => (
       <Route path="/" exact>
         <Home />
       </Route>
-      <Route path="/products">
+      <Route path="/products" exact>
         <Catalog />
+      </Route>
+      <Route path="/products/:productId" exact>
+        <ProductDetail />
       </Route>
       <Route path="/admin">
         <Admin />
