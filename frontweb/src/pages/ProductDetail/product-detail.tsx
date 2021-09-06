856a0ddd5323aaf016b24f2bc120aec86126f39d
Grasiela Gomes <produtoradama@gmail.com>
2021-09-06 10:33:37 -0300

    Product detail responsive

--- frontweb/src/pages/ProductDetail/product-detail.tsx
+++ frontweb/src/pages/ProductDetail/product-detail.tsx
@@ -4,24 +4,28 @@ import { ProductPrice } from 'components/ProductPrice/product-price';
 export const ProductDetail = () => {
   return (
     <div className="container">
-      <main className="base-card mt-3">
+      <main className="base-card mt-3 p-3 p-md-4">
         <header>
-          <button type="button" className="btn btn-light mt-3">
+          <button type="button" className="btn btn-light my-3 mt-md-0">
             <span className="text-primary me-2">&#10094;</span>
             <strong className="text-uppercase">voltar</strong>
           </button>
         </header>
-        <div className="d-flex flex-column p-3">
-          <section>
-            <div className="border rounded p-3 mb-3">
-              <img src={productImg} alt="product name" className="w-100" />
+        <div className="d-flex flex-column flex-lg-row gap-lg-4">
+          <section className="w-100">
+            <div className="d-grid border rounded mb-3">
+              <div className="col-md-6 col-lg-8 mx-auto p-3">
+                <img src={productImg} alt="product name" className="w-100" />
+              </div>
             </div>
-            <article>
-              <h3>Computador Desktop - Intel Core i7</h3>
+            <article className="d-flex flex-column flex-md-row flex-lg-column">
+              <h3 className="col-md-6 col-lg-12 me-md-5 me-lg-0">
+                Computador Desktop - Intel Core i7
+              </h3>
               <ProductPrice price={2779} />
             </article>
           </section>
-          <section className="border rounded p-3 text-black-50 mt-3">
+          <section className="border rounded p-3 text-black-50 mt-3 mt-lg-0 col-lg-6">
             <h6>Descrição do produto</h6>
             <p className="mt-3">
               Projetado para garantir a produtividade no seu dia a dia O
